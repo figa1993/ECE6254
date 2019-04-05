05f6ae3acc377338198568a2013a0393d13958c9
Filip Gajowniczek <filip.r.gajowniczek@gmail.com>
2019-04-05 11:10:13 -0500

    simple code that finds HOG of a window

--- Source/main.py
+++ Source/main.py
@@ -1,3 +1,7 @@
+from skimage.feature import hog
+from scipy.misc import imread,imresize
+import matplotlib.pyplot as plt
+
 ######Script Entry Point##########
 if __name__ == '__main__':
     ########## Model Training ##########
@@ -7,6 +11,16 @@ if __name__ == '__main__':
     # For each image in training set
         ##### Image Pre-processing #####
         ##### Feature Extraction #####
+    image_path = '../Data/Vehicules1024/00000001_ir.png'
+    image = imread(image_path )
+    image_window = image[99:199,99:199 ] # random block
+    fd, hog_image = hog(image_window, orientations=8, pixels_per_cell=(10, 10),
+                    cells_per_block=(10, 10), visualize=True)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+    ax1.imshow(image_window, cmap=plt.cm.gray)
+    ax2.imshow(hog_image, cmap=plt.cm.gray)
+    plt.show()
 
     ##### SVM Training #####
 
