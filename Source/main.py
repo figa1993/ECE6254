759bb3c8d79d66e6464a3f0939b4f58462d6ad25
moalani <49099141+moalani@users.noreply.github.com>
2019-04-22 17:08:38 -0400

    Add files via upload
    Changes:
    classification.py: migrated code from notebook to .py, segmented each classification method as a function, printed metrics at the end (accuracy and precision and recall with curve)
    main.py: integrated classification into main with user input to choose classification method

--- Source/main.py
+++ Source/main.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-
+import numpy as np
 from preprocessing_Iteration_1 import preprocess_images
 from parseAnnotations import parse_annotation_csv
 from parseAnnotations import generate_training_images
 from feature_extraction import generate_training_feature_vectors
+from classification import GridSearch
+from classification import SVM
+from classification import seq
+from classification import process
+
 
 ######Script Entry Point##########
 if __name__ == '__main__':
 
     ###########  Define Directory/File and paths ##########
     # Define where input data resides
-    data_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath('__file__')), "../Data"))
+    data_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath('__file__')), "Vehicules1024"))
 
     # Define where input images reside
     input_image_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath('__file__')),
-                                                                                        "../Data/Vehicules1024"+os.sep))
+                                                                                        "Vehicules1024"+os.sep))
 
     # Define where images output from pre-processing algorithm will be written
     preprocessed_images_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath('__file__')),
@@ -53,6 +58,33 @@ if __name__ == '__main__':
         feature_df = generate_training_feature_vectors( training_dir )
     else:
         feature_df = pd.read_csv(training_features)
+
+    user_input = input('Perform Classification (y/n)?')
+    if (user_input == "y"):
+        data = process()
+        c0 =data[0]
+        c1=data[1]
+        X_train_0, X_test_0, y_train_0, y_test_0 = c0[0],c0[1],c0[2],c0[3]
+        X_train_1, X_test_1, y_train_1, y_test_1 = c1[0], c1[1], c1[2], c1[3]
+
+
+
+        user_input = input('Perform SVM Classification (y/n)?')
+
+        if (user_input == "y"):
+            SVM(X_train_0, X_test_0, y_train_0, y_test_0)
+
+
+        user_input = input('Perform Grid Search Classification (this can take a while) (y/n)? ')
+
+        if (user_input == "y"):
+            GridSearch(X_train_0, X_test_0, y_train_0, y_test_0)
+
+        user_input = input('Perform Sequential Classification (y/n)?')
+
+        if (user_input == "y"):
+            seq(X_train_1, X_test_1, y_train_0, y_test_0)
+
 
 
     # For each image in training set
